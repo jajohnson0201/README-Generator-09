e20d334f913cf65f62c2d40c6b89e4c8d972e1fd
joe <jajohnson0201@gmail.com>
2023-01-01 15:04:23 -0600

    removed seperator bug

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -15,19 +15,20 @@ if(license){return `License: https://opensource.org/licenses/${license}`
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
- return `## License
+  if(license){ return `## License
  ${license}
  ${renderLicenseBadge(license)}
  ${renderLicenseLink(license)}`
+}else{return ""}
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
 
 
-  return  `# ${data.title} 
+return  `# ${data.title} 
 
-  ${renderLicenseBadge(data.license)}
+${renderLicenseBadge(data.license)}
 ## Description
 -${data.description1}
 -${data.description2}
